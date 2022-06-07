5269499d3e758d410c9715b0640d707d286daec9
Romain Jupille <romain.jupille@gmail.com>
2022-06-07 14:29:34 +0200

    Initial push

---
+++ TaxiFareModel/trainer.py
@@ -0,0 +1,73 @@
+
+#external imports
+import pandas as pd
+
+from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+#internal methods imports
+from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
+from TaxiFareModel.utils import compute_rmse
+from TaxiFareModel.data import get_data, clean_data
+
+class Trainer():
+    def __init__(self, X, y):
+        """
+            X: pandas DataFrame
+            y: pandas Series
+        """
+        self.pipeline = None
+        self.X = X
+        self.y = y
+
+    def set_pipeline(self):
+        '''returns a pipelined model'''
+        dist_pipe = Pipeline([
+            ('dist_trans', DistanceTransformer()),
+            ('stdscaler', StandardScaler())
+        ])
+        time_pipe = Pipeline([
+            ('time_enc', TimeFeaturesEncoder('pickup_datetime')),
+            ('ohe', OneHotEncoder(handle_unknown='ignore'))
+        ])
+        preproc_pipe = ColumnTransformer([
+            ('distance', dist_pipe, ["pickup_latitude", "pickup_longitude", 'dropoff_latitude', 'dropoff_longitude']),
+            ('time', time_pipe, ['pickup_datetime'])
+        ], remainder="drop")
+        self.pipeline = Pipeline([
+            ('preproc', preproc_pipe),
+            ('linear_model', LinearRegression())
+        ])
+
+        return self
+
+    def run(self):
+        '''returns a trained pipelined model'''
+        self.set_pipeline()
+        self.pipeline.fit(self.X, self.y)
+        return self
+
+    def evaluate(self, X_eval = None, y_eval = None ):
+        '''returns the value of the RMSE'''
+        if X_eval is None:
+            X_eval = self.X
+        if y_eval is None:
+            y_eval = self.y
+
+        y_pred = self.pipeline.predict(X_eval)
+        rmse = compute_rmse(y_pred, y_eval)
+        return rmse
+
+if __name__ == "__main__":
+    df = get_data()
+    df = clean_data(df)
+    y = df.pop("fare_amount")
+    X = df
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    model = Trainer(X_train, y_train)
+    model.run()
+    result = model.evaluate()
+    print(result)
